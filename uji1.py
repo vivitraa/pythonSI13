464a18647437e4f879489199deb24e6d4c936364
vivitraa <vitra.yolanda06@gmail.com>
2015-09-01 15:17:33 +0700

    first commit

---
+++ uji1.py
@@ -0,0 +1,4 @@
+print "selamat datang"
+nama = raw_input("Masukkan nama anda : ")
+umur = input("Masukkan umur anda : ")
+print "Halo {0}, umur anda {1} tahun".format(nama,umur)
---
+++ uji2.py
@@ -0,0 +1,6 @@
+print "Mari menghitung luas segitiga"
+alas = input("Masukkan alas : ")
+tinggi = input("Masukkan tinggi : ")
+luas = alas*tinggi/2
+print "Luas segitiga adalah {0}".format(luas)
+print "Luas segitiga adalah {0}".format(alas*tinggi/2)
